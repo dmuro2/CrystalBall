06807e266def1038e65638beacdb7fafe92caeba
muro <dmuro2@gmail.com>
2013-11-02 12:50:43 -0700

    Add updates
    Modified the app to accept shake detection.

--- CrystalBall/src/main/java/com/noworknoplay/crystalball/MainActivity.java
+++ CrystalBall/src/main/java/com/noworknoplay/crystalball/MainActivity.java
@@ -2,6 +2,9 @@ package com.noworknoplay.crystalball;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.graphics.drawable.AnimationDrawable;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,18 +13,17 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AlphaAnimation;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.graphics.drawable.AnimationDrawable;
-import android.media.MediaPlayer;
 
 public class MainActivity extends Activity {
 
     private CrystallBall mCrystalBall = new CrystallBall();
     private TextView mAnswerLabel;
-    private Button mGetAnswerButton;
     private ImageView mCrystalBallImage;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private com.example.crystal.ball.ShakeDetector mShakeDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,29 +32,47 @@ public class MainActivity extends Activity {
 
         // Assign the views from the layout file
         mAnswerLabel = (TextView) findViewById(R.id.textView);
-        mGetAnswerButton = (Button) findViewById(R.id.button);
         mCrystalBallImage = (ImageView) findViewById(R.id.imageView);
 
-        mGetAnswerButton.setOnClickListener(new View.OnClickListener() {
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new com.example.crystal.ball.ShakeDetector(new com.example.crystal.ball.ShakeDetector.OnShakeListener() {
+
             @Override
-            public void onClick(View view) {
-
-                String answer = mCrystalBall.getAnAnswer();
-
-                // Update the label with our dynamic answer
-                mAnswerLabel.setText(answer);
-
-                animateCrystalBall();
-                animateAnswer();
-                playSound();
+            public void onShake() {
+                handleNewAnswer();
             }
         });
+
 
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer,SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(mShakeDetector);
+    }
+
+    private void handleNewAnswer() {
+        String answer = mCrystalBall.getAnAnswer();
+
+        // Update the label with our dynamic answer
+        mAnswerLabel.setText(answer);
+
+        animateCrystalBall();
+        animateAnswer();
+        playSound();
     }
 
     private void animateCrystalBall() {
